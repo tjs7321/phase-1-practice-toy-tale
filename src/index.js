8c7cf4abe2a08fea60b3f52c55fd57c6ef9bf273
Theodore Smith <tjs7321@gmail.com>
2023-07-18 19:13:24 -0400

    Got it working

--- src/index.js
+++ src/index.js
@@ -14,53 +14,85 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-
-  ///////create newToyCard///////
-  // const newToyCard = document.createElement("div")
-  // newToyCard.class = "card"
-  // const newToyHeader = document.createElement("h2")
-  // newToyHeader.innerText = "toyName"
-  // const newToyImage = document.createElement("img")
-  // newToyImage.src = "https://www.cnet.com/a/img/resize/0165e20206047e2ebe322821b4fea5fdff7f3061/hub/2014/12/01/89601d70-ca65-4dc7-adfe-f6dbd46d05ba/hobbit-lotr-5-armies-gandalf-hat.jpg?auto=webp&fit=crop&height=675&width=1200"
-  // newToyImage.class = "toy-avatar"
-  // const newToyLikes = document.createElement("p")
-  // newToyLikes.innerText = "Likes"
-  // const newToyButton = document.createElement("button")
-  // newToyButton.class = "like-btn"
-  // newToyButton.id = "[toy_id]"
-  // newToyButton.innerText = "Like ❤️"
-  ///////append elements of newToyCard to div///////
-
-  // newToyCard.appendChild(newToyHeader)
-  // newToyCard.appendChild(newToyImage)
-  // newToyCard.appendChild(newToyLikes)
-  // newToyCard.appendChild(newToyButton)
-
-  //console.log(newToyCard)
-
+  
   ///////fetch toy info from server///////
   fetch(toyURL)
-    .then(response => {return response.json()})
-    .then(object => {for (toy of object){
-      //console.log(toy)
-      const newToyCard = document.createElement("div")
-      newToyCard.classList.add("card")
-      const newToyHeader = document.createElement("h2")
-      newToyHeader.innerText = toy.name
-      const newToyImage = document.createElement("img")
-      newToyImage.src = toy.image
-      newToyImage.classList.add("toy-avatar")
-      const newToyLikes = document.createElement("p")
-      newToyLikes.innerText = toy.likes + " likes"
-      const newToyButton = document.createElement("button")
-      newToyButton.classList.add("like-btn")
-      newToyButton.id = "[toy_id]"
-      newToyButton.innerText = "Like ❤️"
-      newToyCard.appendChild(newToyHeader)
-      newToyCard.appendChild(newToyImage)
-      newToyCard.appendChild(newToyLikes)
-      newToyCard.appendChild(newToyButton)
-      console.log(newToyCard)
-      toyCollection.appendChild(newToyCard)
-    }})
+  .then(response => {return response.json()})
+  ///////iterates through toy array and returns each toy///////
+  .then(object => {for (toy of object){
+    renderNewToy(toy)
+  }})
 });
+
+const updateToyLikes = (toy, newToyLikes) => {
+  const patchRequest = {
+    method: 'PATCH',
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      likes: toy.likes
+    })
+  }
+  fetch(toyURL + '/' + toy.id, patchRequest)
+  .then(response => {return response.json()})
+  .then(object => {newToyLikes.innerText = object.likes + " likes"})
+}
+const renderNewToy = (toy) => {
+    ///////creates new card for each toy///////
+    const newToyCard = document.createElement("div")
+    newToyCard.classList.add("card")
+    const newToyHeader = document.createElement("h2")
+    newToyHeader.innerText = toy.name
+    const newToyImage = document.createElement("img")
+    newToyImage.src = toy.image
+    newToyImage.classList.add("toy-avatar")
+    const newToyLikes = document.createElement("p")
+    newToyLikes.innerText = toy.likes + " likes"
+    const newToyButton = document.createElement("button")
+    newToyButton.classList.add("like-btn")
+    newToyButton.setAttribute("id", toy.id)
+    newToyButton.innerText = "Like ❤️"
+    newToyButton.onclick = () => {
+      toy.likes += 1
+      updateToyLikes(toy, newToyLikes)
+      //console.log(toy.likes)
+    }
+    ///////appends each item to the toy card///////
+    newToyCard.appendChild(newToyHeader)
+    newToyCard.appendChild(newToyImage)
+    newToyCard.appendChild(newToyLikes)
+    newToyCard.appendChild(newToyButton)
+    //console.log(newToyCard)
+    toyCollection.appendChild(newToyCard)
+  }
+
+  const newToyForm = document.getElementById("new-toy-form")
+  
+  newToyForm.onsubmit = (e) => {
+    e.preventDefault()
+    const toyNameInput = document.getElementById("new-toy-name")
+    const toyImageInput = document.getElementById("new-toy-image")
+    const postData = {
+      name: toyNameInput.value,
+      image: toyImageInput.value,
+      likes: 0
+    }
+    //console.log(postData)
+    const configObject = {
+    method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(postData),
+  }
+  fetch(toyURL, configObject)
+  .then(response => {return response.json()})
+  .then(object => {renderNewToy(object)})
+
+
+//////////update toy likes///////////
+
+} 
